07350eadb2071be7cac33c7e0985148a0a71b975
mmgrzenkowicz <50054521+mmgrzenkowicz@users.noreply.github.com>
2019-04-28 09:43:01 +0200

    Upload file Instrukcja SELECT.sql

---
+++ Instrukcja SELECT.sql
@@ -0,0 +1,13 @@
+SELECT * FROM sakila.actor; # Wybierz wrzystko z tabeli actor.
+
+# Rodzaje komentarzy:
+# 1. Komentarz jednowierszowy ( -- ),
+# 2. Komentarz wielowierszowy ( /* */ ),
+# 3. Komentarz specyficzny dla MySQL ( # ).
+
+DESCRIBE sakila.actor; # Opis zawartości tabeli wraz z typami, kluczami oraz extra informacjami.
+
+SELECT first_name, last_name FROM actor;
+
+SELECT first_name, last_name FROM sakila.actor ORDER BY last_name; # Posortowanie po nazwisku.
+
