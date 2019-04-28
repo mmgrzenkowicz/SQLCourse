033cc8d8c0d1e896e6264c128b28560fa0baa940
mmgrzenkowicz <50054521+mmgrzenkowicz@users.noreply.github.com>
2019-04-28 11:47:25 +0200

    Add file Instrukcja UPDATE.sql

---
+++ Instrukcja UPDATE.sql
@@ -0,0 +1,17 @@
+SELECT * FROM sakila.actor WHERE actor_id = 201;
+
+UPDATE sakila.actor SET first_name = 'Bogusław' WHERE actor_id = 201; # Aktualizacja imienia.
+
+SELECT * FROM sakila.actor WHERE actor_id = 201;
+
+SELECT * FROM sakila.actor;
+
+UPDATE sakila.actor SET first_name = 'Pola', last_name = 'Roksa' WHERE actor_id = 200;
+
+SELECT * FROM sakila.actor;
+
+
+
+
+
+
