c9adcace5b352eaa16dd1b9c8a3911f39128a4ca
mmgrzenkowicz <50054521+mmgrzenkowicz@users.noreply.github.com>
2019-04-28 11:28:36 +0200

    Upload file Instrukcja INSERT.sql

---
+++ Instrukcja INSERT.sql
@@ -0,0 +1,3 @@
+INSERT INTO sakila.actor VALUES (201, 'Boguslaw', 'Linda', '2019-04-07 09:45:20'); # Dodanie nowego wiersza.
+
+INSERT INTO sakila.actor(first_name, last_name) VALUES ('Krystyna', 'Janda'); # Dodanie nowego wiersza (Id i Data są ustawione automatycznie).
