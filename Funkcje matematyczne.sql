999afe72b4f1a1ab9470a48db8b44198d32e12b0
mmgrzenkowicz <50054521+mmgrzenkowicz@users.noreply.github.com>
2019-04-30 20:26:55 +0200

    Add file Funkcje matematyczne.sql

---
+++ Funkcje matematyczne.sql
@@ -0,0 +1,25 @@
+SELECT POWER(2,3); # 8
+
+SELECT SQRT(9); # 3
+
+SELECT 5 % 2; # 1
+SELECT 5 MOD 2; # 1
+
+SELECT ABS(3); # 3
+
+SELECT SIGN(-10); # -1
+SELECT SIGN(0); # 0
+SELECT SIGN(5); # 1
+
+SELECT CONV(21, 10, 2); # Przekonwertowanie liczby 21 z systemu dziesiętnego na dwójkowy.
+
+SELECT PI();
+
+SELECT ROUND(2.1412366985,3); # 2.141
+SELECT TRUNCATE(2.1412366985,3); # 2.141
+
+SELECT CEIL(1.123); # 2, zaookrąglenie "do góry"
+
+SELECT FLOOR(1.99); # 1, zaookrąglenie "na dół"
+
+SELECT RAND(); # Funkcja losująca dowolną liczbę
